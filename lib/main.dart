2873732c61e13c081987ab509250e78646ba324b
Claudio Moscoso <cmoscosoz@gmail.com>
2025-02-13 00:05:48 +1300

    Remove BannerSection widget and disable debug banner in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dev_test_2degrees/banner_section.dart';
 import 'package:dev_test_2degrees/src/cubits/address_qualification/address_qualification_cubit.dart';
 import 'package:dev_test_2degrees/src/cubits/address_search/address_search_cubit.dart';
 import 'package:dev_test_2degrees/src/di/service_locator.dart';
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Broadband Search',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
@@ -61,53 +63,6 @@ class Providers extends StatelessWidget {
         ),
       ],
       child: child,
-    );
-  }
-}
-
-class BannerSection extends StatelessWidget {
-  const BannerSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Unlimited Broadband from\n\$50 a month",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 8),
-          const Text(
-            "When you join us on a 12-month plan.",
-            style: TextStyle(color: Colors.white, fontSize: 16),
-          ),
-          const SizedBox(height: 8),
-          Row(
-            children: [
-              Expanded(
-                child: const Text(
-                  "SAY WHAAAAAAAAAT?!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 8),
-              Text("😜", style: TextStyle(fontSize: 32)),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
